95e7b1ed5db36b5113a12473e3a94948ea718ed4
George.P <georgipetroov@gmail.com>
2022-02-21 17:18:20 +0200

    main updated

--- java-academy-22-intro/src/course/academy/Main.java
+++ java-academy-22-intro/src/course/academy/Main.java
@@ -1,7 +1,25 @@
 package course.academy;
 
+import course.academy.dao.BookRepository;
+import course.academy.dao.exception.InvalidRepositoryStateException;
+import course.academy.dao.impl.BookRepositoryImpl;
+import course.academy.model.Book;
+import course.academy.model.MockBooks;
+
 public class Main {
     public static void main(String[] args) {
+        BookRepository bookRepository = new BookRepositoryImpl();
+        for (Book book : MockBooks.MOCK_BOOKS) {
+            try {
+                bookRepository.create(book);
+            } catch (InvalidRepositoryStateException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
 
+        for (Book book : bookRepository.findAll()) {
+            System.out.println(book);
+        }
     }
 }
